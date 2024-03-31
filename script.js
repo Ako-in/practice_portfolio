fbb69c5c5e296ef41962ba03c76cc01d21d5462d
Ako-in <sinba.805i@gmail.com>
2024-03-31 15:32:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(function(){
+//   $('#image').on('mouseover',function(){
+//     $(this).animate({
+//       opacity:1.0,
+//     });
+//   });
+// });
+
+$(function(){
+  $('#image').on('click',function(){
+    $(this).animate({
+      opacity:1.0,
+    });
+  });
+});
