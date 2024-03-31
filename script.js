628382d573ddd144ad6ab2d51f6069385ad1d666
Ako-in <sinba.805i@gmail.com>
2024-03-31 21:49:15 +0700

    CSS修正、JSファイルの追加 アイコン画像の枠

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+console.log('111');
+// Worksセクションの画像にカーソルが当たった時
 // $(function(){
 //   $('#image').on('mouseover',function(){
+//     console.log('111');
 //     $(this).animate({
 //       opacity:1.0,
 //     });
 //   });
 // });
 
-$(function(){
-  $('#image').on('click',function(){
-    $(this).animate({
-      opacity:1.0,
-    });
-  });
+$('#hover').mouseover(function(){
+  console.log('222');
+})
+
+$('#image').mouseover(function() {
+
+  console.log('マウスオーバーしました！');
+
 });
+
+
+
+// カルーセル
+$('.carousel').slick({
+  autoplay: true,
+  dots: true,
+  infinite: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+});
+
